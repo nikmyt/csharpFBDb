ab5f9271d9b7ea35b58e85d269e8325e2623af5f
koon-dev <kylienikka@seznam.cz>
2022-11-15 09:29:56 +0100

    Working MVVM type something (todo: view change)
    Also cleaned out the corpses of unsuccessors.

---
+++ FBDb/Core/RelayCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+//this does something
+
+namespace WPF.Core
+{
+	internal class RelayCommand : ICommand
+	{
+		private Action<Object> _execute;
+		private Func<object, bool> _canExecute;
+
+		public event EventHandler CanExecuteChanged
+		{
+			add { CommandManager.RequerySuggested += value; }
+			remove { CommandManager.RequerySuggested -= value; }
+		}
+
+		public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+		{
+			_execute = execute;
+			_canExecute = canExecute;
+		}
+
+		public bool CanExecute(object parameter)
+		{
+			return _canExecute == null || _canExecute(parameter);
+		}
+
+		public void Execute(object parameter)
+		{
+			_execute(parameter);
+		}
+	}
+
+
+
+}
---
+++ FBDb/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF.Core;
+
+namespace WPF.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        private object _currentView;
+
+        public HomeViewModel HomeVM { get; set; }
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            CurrentView = HomeVM;
+        }
+    }
+}
